8dd4e198191009d0571755cf3285a7f73cf2a2d3
Lucas Fernando <73972922+DigitalHDR@users.noreply.github.com>
2022-05-09 17:19:30 -0300

    6° passando o store para o carrinho com os item que estiverem armazenado nele

--- src/pages/Carrinho/index.jsx
+++ src/pages/Carrinho/index.jsx
@@ -1,3 +1,4 @@
+//scr/pages/carrinho
 import React from 'react';
 import Botao from '../../components/Botao';
 import './styles.css';
@@ -5,48 +6,43 @@ import { MdAdd, MdRemove, MdDeleteOutline } from 'react-icons/md';
 import { useSelector } from 'react-redux';
 
 export default function Carrinho() {
-  const itensAdicionados = useSelector((state) => state.carrinho)
-
-  console.log('mues itens', itensAdicionados)
-
+  const itensAdicionados = useSelector((state) => state.carrinho);
   return (
     <div className="container_global">
       <div className="container_cart_carrinho">
-        <div className="card_carrinho">
-          <div className="container_img_carrinho">
-            <img src="https://i.postimg.cc/3JM0KMv3/14.png" alt="s" />
-          </div>
-
-          <div className="container_descricao_carrinho">
-            <div className="box_titulo_descricao">
-              <div className="box_titulo_btnDelete">
-                <h3 className="titulo_carrinho">titulo do produto</h3>
-                <MdDeleteOutline className="btnDelete" />
-              </div>
-              <p>
-                descrição descrição descrição descrição descrição descrição{' '}
-                descrição descrição descrição descrição descrição descrição{' '}
-                descrição descrição descrição descrição descrição descrição{' '}
-              </p>
+        {itensAdicionados.map((item) => (
+          <div className="card_carrinho">
+            <div className="container_img_carrinho">
+              <img src={item.imagem} alt={item.titulo} />
             </div>
 
-            <div className="box_preco_btn">
-              <div>
-                <span className="preco_carrinho">R$11.11</span>
+            <div className="container_descricao_carrinho">
+              <div className="box_titulo_descricao">
+                <div className="box_titulo_btnDelete">
+                  <h3 className="titulo_carrinho">{item.titulo}</h3>
+                  <MdDeleteOutline className="btnDelete" />
+                </div>
+                <p>{item.descricao}</p>
               </div>
 
-              <div className="btn_carrinho">
-                <Botao btn={global}>
-                  <MdRemove className="btnRemove" />
-                </Botao>
-                <input type="text" readOnly value={12345} />
-                <Botao btn={global}>
-                  <MdAdd className="btnAdd" />
-                </Botao>
+              <div className="box_preco_btn">
+                <div>
+                  <span className="preco_carrinho">{item.preco}</span>
+                </div>
+
+                <div className="btn_carrinho">
+                  <Botao btn={global}>
+                    <MdRemove className="btnRemove" />
+                  </Botao>
+                  <input type="text" readOnly value={item.quantidade} />
+                  <Botao btn={global}>
+                    <MdAdd className="btnAdd" />
+                  </Botao>
+                </div>
               </div>
             </div>
           </div>
-        </div>
+        ))}
       </div>
     </div>
   );
