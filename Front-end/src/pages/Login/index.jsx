014bf1626f80b682d39bf7c5484315f8e6d5717b
Lucas Fernando <73972922+DigitalHDR@users.noreply.github.com>
2022-05-25 17:13:53 -0300

    frontend base do formulario ok

--- Front-end/src/pages/Login/index.jsx
+++ Front-end/src/pages/Login/index.jsx
@@ -1,10 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './style.css'
 import TituloDaPagina from '../../components/TituloDaPagina'
+import BotaoDetalhes from '../../components/BotaoDetalhes'
 
 export default function Login() {
+  const [cadastrado, setCadastrado] = useState(false)
+
   return (
-    <>
-      <TituloDaPagina>Login</TituloDaPagina>
-    </>
+    <div className="container_global">
+      <div className="box_form">
+        <TituloDaPagina>{!cadastrado ? 'Login' : 'Cadastrar-se'}</TituloDaPagina>
+        <form>
+          <label>
+            Nome
+            <input type="text" />
+          </label>
+
+          <label>
+            Email
+            <input type="email" name="" id="" />
+          </label>
+
+          {cadastrado && (
+            <label>
+              Senha
+              <input type="password" name="" id="" />
+            </label>
+          )}
+
+          <div className="box_btn">
+            <BotaoDetalhes>
+              {!cadastrado ? 'Fazer Login' : 'Cadastrar'}
+            </BotaoDetalhes>
+          </div>
+
+          <p className='loginOuCadastro'
+            onClick={() => setCadastrado(!cadastrado)}
+            style={{ color: '#fff', textAlign: 'center' }}
+          >
+            Clique aqui para {!cadastrado ? 'se Cadastrar' : 'Fazer o Login'}
+          </p>
+        </form>
+      </div>
+    </div>
   )
 }
--- Front-end/src/routes.js
+++ Front-end/src/routes.js
@@ -1,14 +1,14 @@
 //src/routes
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
 
-import Home from './pages/Home';
-import Produtos from './pages/Produtos';
-import ProdutoDetalhe from './pages/ProdutoDetalhe';
-import Lojas from './pages/Lojas';
-import Sobre from './pages/Sobre';
-import Login from './pages/Login';
-import Carrinho from './pages/Carrinho';
+import Home from './pages/Home'
+import Produtos from './pages/Produtos'
+import ProdutoDetalhe from './pages/ProdutoDetalhe'
+import Lojas from './pages/Lojas'
+import Sobre from './pages/Sobre'
+import Login from './pages/Login'
+import Carrinho from './pages/Carrinho'
 
 export default function Rotas() {
   return (
@@ -23,4 +23,3 @@ export default function Rotas() {
     </Routes>
   )
 }
-
